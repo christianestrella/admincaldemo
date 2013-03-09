223a2bf1517866c9169617116b9b0d3d559eb9d1
christian <christian@H77H2-M3.(none)>
2013-03-09 01:30:21 -0600

    Commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admincaldemo::Application.config.secret_token = 'c66fbee1580f51c5b06d36b6b69bab55c47af5e61a7e76db4384640e059642f67afe87a5b0888866d0ae931ce445ac634a2d38fa7d9a76edaade61e76f924732'
